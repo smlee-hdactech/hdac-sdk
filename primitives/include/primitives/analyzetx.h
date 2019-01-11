798a956935228c8531036481f8eb6962f8939c88
smlee-hdactech <sm.lee4573@hdactech.com>
2019-01-11 16:29:00 +0900

    add analysis for metadata

--- primitives/include/primitives/analyzetx.h
+++ primitives/include/primitives/analyzetx.h
@@ -4,12 +4,6 @@
 #include <string>
 #include <json_spirit/json_spirit.h>
 
-//#define PRINT_CHECK
-
-#ifdef PRINT_CHECK
-void analyzeTx(const std::string& txHex);
-#else
 json_spirit::Object analyzeTx(const std::string& txHex);
-#endif
 
 #endif  //ANALYZETX_H
--- primitives/src/analyzetx.cpp
+++ primitives/src/analyzetx.cpp
@@ -11,12 +11,50 @@
 using namespace std;
 using namespace json_spirit;
 
+std::string analyzeDetail(const std::vector<unsigned char>& vch)
+{
+    string str = strprintf("%s(%s) ", string(vch.begin(), vch.begin()+4), HexStr(vch.begin(), vch.begin()+4));
+
+    switch(vch[3]) {
+    case 'g':   // New issuance quantity
+        break;
+    case 'n':   // New issuance metadata, Stream creation outputs
+        break;
+    case 'q': {  // Per-output asset metadata
+        const int64_t* qty = reinterpret_cast<const int64_t*>(&vch[4+16]);
+        return str + "ASSET-ID(" + HexStr(vch.begin()+4, vch.begin()+4+16) + ") " + "QTY:"+ to_string(*qty) + "(" + HexStr(vch.begin()+4+16, vch.end()) + ")";
+    }
+    case 'o':   // Follow-on issuance quantity
+        break;
+    case 'e':   // Follow-on issuance metadata::asset-identifier, Stream item outputs::stream-identifier
+        return str + "ASSET|STREAM-ID(" + HexStr(vch.begin()+4, vch.end()) + ")";
+    case 'u':   // Follow-on issuance metadata:issue-details
+        return str + "ASSET-TYPE(" + HexStr(vch.begin()+4, vch.begin() + 5) + ") " + "ASSET-PROP(" + HexStr(vch.begin()+5, vch.end()) + ")";
+    case 'k':   // Stream item outputs::item-key
+        return str + "STREAM-KEY:" + string(vch.begin()+4, vch.end()) + "(" + HexStr(vch.begin()+4, vch.end()) + ")";
+    }
+
+    return str + HexStr(vch.begin()+4, vch.end());
+}
+
+bool isSpeciaMeta(const std::vector<unsigned char>& vch)
+{
+    if (string(&vch[0], &vch[3]) == "spk") {
+        return true;
+    }
+    return false;
+}
+
 inline std::string ValueString(const std::vector<unsigned char>& vch)
 {
     if (vch.size() <= 4)
         return strprintf("%d", CScriptNum(vch, false).getint());
-    else
+    else if (isSpeciaMeta(vch)) {
+        return analyzeDetail(vch);
+    }
+    else {
         return HexStr(vch);
+    }
 }
 
 
@@ -46,59 +84,29 @@ std::string analyzeScript(const CScript& script)
     return str;
 }
 
-#ifdef PRINT_CHECK
-string analyzeVin(const CTxIn& vin)
-#else
+
 Object analyzeVin(const CTxIn& vin)
-#endif
 {
-#ifdef PRINT_CHECK
-    string str;
-    str += strprintf("    prevhash=%s, prevout=%08x, \n",
-                     vin.prevout.hash.ToString(),
-                     vin.prevout.n);
-    // TODO : coinbase
-    //string strScriptSig = HexStr(vin.scriptSig);
-    // TODO : maybe scirptSig-size has variable-length
-    str += strprintf("    scriptSig: %s, \n", analyzeScript(vin.scriptSig));
-    str += strprintf("    nSequence=%08x\n", vin.nSequence);
-    return str;
-#else
     Object parsedObj;
     parsedObj.push_back(Pair("prevhash", vin.prevout.hash.ToString()));
     parsedObj.push_back(Pair("prevout", strprintf("%08x", vin.prevout.n)));
     parsedObj.push_back(Pair("scriptSig", analyzeScript(vin.scriptSig)));
     parsedObj.push_back(Pair("nSequece", strprintf("%08x", vin.nSequence)));
     return parsedObj;
-#endif
 }
 
-#ifdef PRINT_CHECK
-string analyzeVout(const CTxOut& vout)
-#else
 Object analyzeVout(const CTxOut& vout)
-#endif
 {
     ostringstream stm;
     stm << setfill('0') << setw(16) << hex << vout.nValue;
-#ifdef PRINT_CHECK
-    string str;
-    str += strprintf("    nValue=%s, ", stm.str());
-    str += strprintf("\n    scriptPubKey: %s\n", analyzeScript(vout.scriptPubKey));
-    return str;
-#else
+
     Object parsedObj;
     parsedObj.push_back(Pair("nValue", stm.str()));
     parsedObj.push_back(Pair("scriptPubKey", analyzeScript(vout.scriptPubKey)));
     return parsedObj;
-#endif
 }
 
-#ifdef PRINT_CHECK
-void analyzeTx(const string& txHex)
-#else
 Object analyzeTx(const string& txHex)
-#endif
 {    
     CTransaction tx;
     vector<unsigned char> txData(ParseHex(txHex));
@@ -108,62 +116,25 @@ Object analyzeTx(const string& txHex)
     }
     catch (const std::exception &) {
         cerr << "txHex is not transaction" << endl;
-#ifdef PRINT_CHECK
-        return;
-#else
         return Object();
-#endif
     }
 
-#ifdef PRINT_CHECK
-    cout << "raw: " << txHex << endl;
-
-    cout << "parsed: " << endl;
-
-    // TODO : vin-size has variable length
-    std::string str;
-    str += strprintf("ver=%08x, vin-count=%02x\n",
-        tx.nVersion,
-        tx.vin.size()
-        );
-#else
     Object parsedObj;
     parsedObj.push_back(Pair("version", strprintf("%08x", tx.nVersion)));
     parsedObj.push_back(Pair("vin-count", strprintf("%02x", tx.vin.size())));
-#endif
 
     for (unsigned int i = 0; i < tx.vin.size(); i++) {
-#ifdef PRINT_CHECK
-        str += strprintf("vin[%d]: \n", i);
-        str += analyzeVin(tx.vin[i]);
-#else
         parsedObj.push_back(Pair(strprintf("vin[%d]", i), analyzeVin(tx.vin[i])));
-#endif
     }
 
     // TODO : vout-size has variable length
-#ifdef PRINT_CHECK
-    str += strprintf("vout-count=%02x\n", tx.vout.size());
-#else
     parsedObj.push_back(Pair("vout-count", strprintf("%02x", tx.vout.size())));
-#endif
 
     for (unsigned int i = 0; i < tx.vout.size(); i++) {
-#ifdef PRINT_CHECK
-        str += strprintf("vout[%d]: \n", i);
-        str += analyzeVout(tx.vout[i]);
-#else
         parsedObj.push_back(Pair(strprintf("vout[%d]", i), analyzeVout(tx.vout[i])));
-#endif
     }
 
-#ifdef PRINT_CHECK
-    str += strprintf("nLockTime=%08x", tx.nLockTime);
-    cout << str << endl;
-    //cout << tx.ToString();
-#else
     parsedObj.push_back(Pair("nLockTime", strprintf("%08x", tx.nLockTime)));
     return parsedObj;
-#endif
 }
 
