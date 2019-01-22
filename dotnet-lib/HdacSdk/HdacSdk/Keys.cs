0bf63da18b4a34273bd154d837b08abafdd3d8f0
moony <sm.lee4573@hd-bsnc.com>
2019-01-22 17:34:06 +0900

    .net을 위한 라이브러리 및 테스트 프로그램 초기버전

---
+++ dotnet-lib/HdacSdk/HdacSdk/Keys.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HdacSdk
+{
+    public class Keys
+    {
+        [DllImport("keys_wrapper.dll", CallingConvention = CallingConvention.Cdecl)]
+        extern public static void test_char_param([MarshalAs(UnmanagedType.LPStr)]String streamKey);
+                
+        [DllImport("keys_wrapper.dll", CallingConvention = CallingConvention.Cdecl)]
+        extern public static void create_stream_publish_tx1([MarshalAs(UnmanagedType.LPStr)]String streamKey, [MarshalAs(UnmanagedType.LPStr)]String streamItem,
+            [MarshalAs(UnmanagedType.LPStr)]String createTxid, [MarshalAs(UnmanagedType.LPStr)]String unspentScriptPubKey,
+            [MarshalAs(UnmanagedType.LPStr)]String unspentTxid, uint unspentVOut,
+            [MarshalAs(UnmanagedType.LPStr)]String unspentRedeemScript, [MarshalAs(UnmanagedType.LPStr)]String privateKey,
+            [MarshalAs(UnmanagedType.LPStr)]String privateKeyPrefix, [MarshalAs(UnmanagedType.LPStr)]String addrChecksum);
+    }
+}
