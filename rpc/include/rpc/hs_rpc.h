c9f7a7dc4bcc181063b77a9d9008d3230126c502
moony <sm.lee4573@hd-bsnc.com>
2019-01-24 10:03:40 +0900

    doxygen format에 맞는 주석 처리

--- rpc/include/rpc/hs_rpc.h
+++ rpc/include/rpc/hs_rpc.h
@@ -1,11 +1,4 @@
-﻿/**
-* @file		hs_rpc.h
-* @date		2019-01-17
-* @author	HDAC Technology Inc.
-*
-* @brief	hs_rpc 헤더 파일.
-*/
-
+﻿
 #ifndef HS_RPC_H
 #define HS_RPC_H
 
@@ -15,32 +8,44 @@ class RpcClient;
 
 /// RPC 명령어를 통하여 HDAC 네트워크의 기본 정보를 가져 온다.
 json_spirit::Object getinfo(const RpcClient& client);
+
 /// RPC 명령어를 통하여 HDAC 네트워크의 파라미터 설정 정보를 가져 온다.
 json_spirit::Object blockChainParams(const RpcClient& client);
+
 /// RPC 명령어를 통하여 아직 보내지 못한 transaction 들의 정보를 가져 온다.
-json_spirit::Object listunspent(const RpcClient& client, int minConf = 1, int maxConf = 9999999,
-                                const std::vector<std::string>& addresses = std::vector<std::string>{});
+json_spirit::Object listunspent(const RpcClient& client, int minConf = 1, int maxConf = 9999999, 
+	const std::vector<std::string>& addresses = std::vector<std::string>{});
+
 /// RPC 명령어를 통하여 아직 보내지 못한 transaction 들의 정보를 가져 온다.
 json_spirit::Object listunspent(const RpcClient& client, const std::vector<std::string>& addresses);
+
 /// RPC 명령어를 통하여 아직 보내지 못한 transaction 들의 정보를 가져 온다.
 json_spirit::Object listunspent(const RpcClient& client, const std::string& address);
+
 /// RPC 명령어를 통하여 아직 보내지 못한 transaction 을 잠금 하거나 또는 잠금 해제 할 수 있게 한다.
 json_spirit::Object lockunspent(const RpcClient& client, bool unlock, std::string txid, int vout);
+
 /// RPC 명령어를 통하여 아직 보내지 못한 transaction 들 중에 잠겨 있는 transaction 들의 정보를 가져 온다.
 json_spirit::Object listlockunspent(const RpcClient& client);
+
 /// RPC 명령어를 통하여 생성 된 stream 들의 정보를 가져 온다.
 json_spirit::Object liststreams(const RpcClient& client, const std::vector<std::string> &streamNames);
+
 /// RPC 명령어를 통하여 생성 된 stream 들의 정보를 가져 온다.
 json_spirit::Object liststreams(const RpcClient& client, const std::string &streamName = "all");
+
 /// RPC 명령어를 통하여 정의 된 asset 들의 정보를 가져온다.
 json_spirit::Object listassets(const RpcClient& client, const std::vector<std::string> &assetNames);
+
 /// RPC 명령어를 통하여 정의 된 asset 들의 정보를 가져온다.
 json_spirit::Object listassets(const RpcClient& client, const std::string &name = "");
+
 /// RPC 명령어를 통하여 sign 된 메시지를 검증 한다.
 json_spirit::Object verifymessage(const RpcClient& client,
 			const std::string &strAddress,
                         const std::string &strSignmessage,
 			const std::string &strText);
+
 /// RPC 명령어를 통하여 개인키를 이용 하여 sign 된 메시지를 만든다.
 json_spirit::Object signmessage(const RpcClient& client,
 			const std::string &strPrivateKey,
@@ -48,6 +53,7 @@ json_spirit::Object signmessage(const RpcClient& client,
 
 /// RPC 명령어를 통하여 특정 지갑주소의 정보를 가져온다.
 json_spirit::Object importaddress(const RpcClient& client, const std::string &address, bool rescan = true);
+
 /// RPC 명령어를 통하여 특정 지갑주소의 정보를 가져온다.
 json_spirit::Object importaddress(const RpcClient& client, const std::vector<std::string>& addresses, bool rescan = true);
 
--- rpc/src/hs_rpc.cpp
+++ rpc/src/hs_rpc.cpp
@@ -21,9 +21,8 @@ using namespace json_spirit;
  * rawtransaction 을 전송 한다. 
  * @details createrawtx 명령어를 통하여 sign 된 16 진수 Hex
  * rawtransaction 을 전송 한다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param const string & rawTx createrawtx 명령어를 통하여 만들어진
- * sign 된 16진수 Hex rawtransaction 문자열
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param rawTx createrawtx 명령어를 통하여 만들어진 sign 된 16진수 Hex rawtransaction 문자열
  *
  * @return sendrawtx RPC 명령의 결과 값, transaction hash 값
  *
@@ -40,10 +39,9 @@ Object sendrawtx(const RpcClient& client, const string& rawTx)
  *
  * @brief RPC 명령어를 통하여 특정 주소의 정보를 가져 온다. 
  * @details rescan 을 통하여 특정 주소의 정보를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param const string & address 정보를 가져올 특정 주소 문자열
- * @param bool rescan 특정 지갑주소의 정보를 가져올 때, rescan 을
- * 수행 할 것인지 아닌지에 대한 여부 (default = true)
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param address 정보를 가져올 특정 주소 문자열
+ * @param rescan 특정 지갑주소의 정보를 가져올 때, rescan을 수행 할 것인지 아닌지에 대한 여부 (default = true)
  *
  * @return importaddress RPC 명령어의 결과 값, (없음)
  *
@@ -59,10 +57,9 @@ Object importaddress(const RpcClient& client, const string &address, bool rescan
  *
  * @brief RPC 명령어를 통하여 특정 주소의 정보를 가져 온다. 
  * @details rescan 을 통하여 특정 주소의 정보를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param const vector<string> & address 정보를 가져올 특정 주소 문자열
- * @param bool rescan 특정 지갑주소의 정보를 가져올 때, rescan 을
- * 수행 할 것인지 아닌지에 대한 여부 (default = true)
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param addresses 정보를 가져올 특정 주소 문자열
+ * @param rescan 특정 지갑주소의 정보를 가져올 때, rescan을 수행 할 것인지 아닌지에 대한 여부 (default = true)
  *
  * @return importaddress RPC 명령어의 결과 값, (없음)
  *
@@ -81,11 +78,10 @@ Object importaddress(const RpcClient& client, const vector<string>& addresses, b
 }
 
 /**
- *
+ * @fn Object getinfo(const RpcClient& client)
  * @brief RPC 명령어를 통하여 HDAC 네트워크의 기본 정보를 가져 온다. 
- * @details HDAC 네트워크의 버전, 블럭 높이, 체인 이름 등의
- * 기본 정보를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
+ * @details HDAC 네트워크의 버전, 블럭 높이, 체인 이름 등의 기본 정보를 가져 온다.
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
  *
  * @return getinfo RPC 명령어의 결과 값, (버전, 블럭 높이,
  * 체인 이름등의 HDAC 네트워크의 기본 정보)
@@ -99,14 +95,12 @@ Object getinfo(const RpcClient& client)
 }
 
 /**
- *
+ * @fn Object blockChainParams(const RpcClient& client)
  * @brief RPC 명령어를 통하여 HDAC 네트워크의 파라미터 설정 정보를 가져 온다. 
- * @details HDAC 네트워크의 지갑 주소 체크섬, 체인 이름, rpc port 등의
- * 파라미터 설정 정보를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
+ * @details HDAC 네트워크의 지갑 주소 체크섬, 체인 이름, rpc port 등의 파라미터 설정 정보를 가져 온다.
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
  *
- * @return blockchainparams RPC 명령어의 결과 값, (지갑 주소 체크섬,
- * 체인 이름, rpc port 등의 HDAC 네트워크의 파라미터 설정 정보)
+ * @return blockchainparams RPC 명령어의 결과 값, (지갑 주소 체크섬, 체인 이름, rpc port 등의 HDAC 네트워크의 파라미터 설정 정보)
  *
  */
 Object blockChainParams(const RpcClient& client)
@@ -115,14 +109,15 @@ Object blockChainParams(const RpcClient& client)
 }
 
 /**
+ * @fn Object listunspent(const RpcClient& client, int minConf, int maxConf, const vector<string>& addresses)
+ * @brief RPC 명령어를 통하여 아직 보내지 못한 transaction 들의 정보를 가져 온다. 
+ * @details HDAC 네트워크의 지갑 주소 체크섬, 체인 이름, rpc port 등의 파라미터 설정 정보를 가져 온다.
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param minConf 최소 컨펌 수(filter 역할, 범위 시작)
+ * @param maxConf 최대 컨펌 수(filter 역할, 범위 끝)
+ * @param addresses 조회하고자 하는 특정 지갑 주소들
  *
- * @brief RPC 명령어를 통하여 HDAC 네트워크의 파라미터 설정 정보를 가져 온다. 
- * @details HDAC 네트워크의 지갑 주소 체크섬, 체인 이름, rpc port 등의
- * 파라미터 설정 정보를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- *
- * @return blockchainparams RPC 명령어의 결과 값, (지갑 주소 체크섬,
- * 체인 이름, rpc port 등의 HDAC 네트워크의 파라미터 설정 정보)
+ * @return listunspent RPC 명령어의 결과 값, (특정 지갑에 보내지 못한 transaction 리스트)
  *
  */
 Object listunspent(const RpcClient& client, int minConf, int maxConf, const vector<string>& addresses)
@@ -152,15 +147,13 @@ Object listunspent(const RpcClient& client, int minConf, int maxConf, const vect
 }
 
 /**
- *
+ * @fn Object listunspent(const RpcClient& client, const vector<string>& addresses)
  * @brief RPC 명령어를 통하여 아직 보내지 못한 transaction 들의 정보를 가져 온다. 
- * @details 특정 지갑주소들에서 생성하여 아직 보내지 못한
- * transaction 들의 정보를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param const vector<string> & addresses 조회하고자 하는 특정 지갑 주소들
+ * @details 특정 지갑주소들에서 생성하여 아직 보내지 못한 transaction 들의 정보를 가져 온다.
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param addresses 조회하고자 하는 특정 지갑 주소들
  *
- * @return listunspent RPC 명령어의 결과 값, (특정 지갑에 보내지 못한
- * transaction 리스트)
+ * @return listunspent RPC 명령어의 결과 값, (특정 지갑에 보내지 못한 transaction 리스트)
  *
  */
 Object listunspent(const RpcClient& client, const vector<string>& addresses)
@@ -169,15 +162,14 @@ Object listunspent(const RpcClient& client, const vector<string>& addresses)
 }
 
 /**
- *
+ * @fn Object listunspent(const RpcClient& client, const string& address)
  * @brief RPC 명령어를 통하여 아직 보내지 못한 transaction 들의 정보를 가져 온다. 
  * @details 특정 지갑주소들에서 생성하여 아직 보내지 못한
  * transaction 들의 정보를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param const string & addresses 조회하고자 하는 특정 지갑 주소들
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param address 조회하고자 하는 특정 지갑 주소들
  *
- * @return listunspent RPC 명령어의 결과 값, (특정 지갑에 보내지 못한
- * transaction 리스트)
+ * @return listunspent RPC 명령어의 결과 값, (특정 지갑에 보내지 못한 transaction 리스트)
  *
  */
 Object listunspent(const RpcClient& client, const string& address)
@@ -186,15 +178,15 @@ Object listunspent(const RpcClient& client, const string& address)
 }
 
 /**
- *
+ * @fn Object lockunspent(const RpcClient& client, bool unlock, string txid, int vout)
  * @brief RPC 명령어를 통하여 아직 보내지 못한 transaction 을 잠금 하거나
  * 또는 잠금 해제 할 수 있게 한다.
  * @details 아직 보내지 못한 transaction 에 대하여 보내지 못 하도록 잠금 하거나
  * 또는 잠겨 있는 transaction 의 잠금을 해제 할 수 있게 한다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param bool unlock 잠금 할 껀지,  잠금 해제 할 껀지  여부
- * @param string txid 잠금 또는 잠금 해제 할 transaction 의 ID
- * @param int vout 잠금 또는 잠금 해제 하고자 하는 transaction 의 vout 양
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param unlock 잠금 할 껀지,  잠금 해제 할 껀지  여부
+ * @param txid 잠금 또는 잠금 해제 할 transaction 의 ID
+ * @param vout 잠금 또는 잠금 해제 하고자 하는 transaction 의 vout 양
  *
  * @return lockunspent RPC 명령어의 결과 값, (잠금 또는 잠금 해제 성공 여부,
  * true, false)
@@ -214,15 +206,13 @@ Object lockunspent(const RpcClient& client, bool unlock, string txid, int vout)
 }
 
 /**
- *
+ * @fn Object listlockunspent(const RpcClient& client)
  * @brief RPC 명령어를 통하여 아직 보내지 못한 transaction 중에 잠겨 있는
  * transaction 들의 정보를 가져 온다.
- * @details 아직 보내지 못한 transaction 들 중에 전송 되지 못하도록 잠겨 있는
- * transaction 들의 정버를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
+ * @details 아직 보내지 못한 transaction 들 중에 전송 되지 못하도록 잠겨 있는 transaction 들의 정버를 가져 온다.
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
  *
- * @return listlockunspent RPC 명령어의 결과 값, (잠금 되어진 transaction 들의
- * ID 와 vout 값)
+ * @return listlockunspent RPC 명령어의 결과 값, (잠금 되어진 transaction 들의 ID 와 vout 값)
  *
  */
 Object listlockunspent(const RpcClient& client)
@@ -234,9 +224,8 @@ Object listlockunspent(const RpcClient& client)
  *
  * @brief RPC 명령어를 통하여 생성 된 stream 들의 정보를 가져 온다.
  * @details 생성 되어진 stream 들의 정보를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param const vector<string> & streamName 특정 stream 의 정보를 가져
- *  올 때 사용 한다.
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param streamNames 특정 stream 의 정보를 가져올 때 사용 한다.
  *
  * @return liststreams RPC 명령어의 결과 값, (생성되어진 stream 정보)
  *
@@ -257,8 +246,8 @@ Object liststreams(const RpcClient& client, const vector<string> &streamNames)
  *
  * @brief RPC 명령어를 통하여 생성 된 stream 들의 정보를 가져 온다.
  * @details 생성 되어진 stream 들의 정보를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param const string & streamName 특정 stream 의 정보를 가져 올 때 사용 한다.
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param streamName 특정 stream 의 정보를 가져 올 때 사용 한다.
  *
  * @return liststreams RPC 명령어의 결과 값, (생성되어진 stream 정보)
  *
@@ -273,9 +262,8 @@ Object liststreams(const RpcClient& client, const string &streamName)
  *
  * @brief RPC 명령어를 통하여 정의 된 asset 들의 정보를 가져 온다.
  * @details 정의 되어진 asset 들의 정보를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param const vector<string> & assetNames 특정 asset 들의 정보를 가져
- * 올 때 사용 한다.
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param assetNames 특정 asset 들의 정보를 가져올 때 사용 한다.
  *
  * @return listassets RPC 명령어의 결과 값, (생성되어진 asset 정보)
  *
@@ -296,8 +284,8 @@ Object listassets(const RpcClient& client, const vector<string> &assetNames)
  *
  * @brief RPC 명령어를 통하여 정의 된 asset 들의 정보를 가져 온다.
  * @details 정의 되어진 asset 들의 정보를 가져 온다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param const string & name 특정 asset 들의 정보를 가져
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param name 특정 asset 들의 정보를 가져
  * 올 때 사용 한다.
  *
  * @return listassets RPC 명령어의 결과 값, (생성되어진 asset 정보)
@@ -316,9 +304,9 @@ Object listassets(const RpcClient& client, const string &name)
  *
  * @brief RPC 명령어를 통하여 개인키를 이용 하여 sign 된 메시지를 만든다.
  * @details 개인키를 이용하여 평문의 문자열을 sign 된 문자열로 변환 한다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param const string & strPrivateKey sign 할려고하는 개인키 값
- * @param const string & strText sign 할려고하는 평문의 문자열
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param strPrivateKey sign 할려고하는 개인키 값
+ * @param strText sign 할려고하는 평문의 문자열
  *
  * @return signmessage RPC 명령어의 결과 값, (생성되어진 sign 메시지 문자열)
  *
@@ -340,12 +328,11 @@ Object signmessage(const RpcClient& client, const string &strPrivateKey,
 /**
  *
  * @brief RPC 명령어를 통하여 sign 된 메시지를 검증 한다.
- * @details signmessage 를 통하여 sign 된 메시지가 제대로 sign 되었는지
- * 검증 한다.
- * @param const RpcClient & client HDAC 네트워크 접속 정보를 제공한다.
- * @param const string & strAddress sign 할 때 사용 된 지갑 주소
- * @param const string & strSignmessage sign 되어진 문자열
- * @param const string & strText sign 한 원본 문자열
+ * @details signmessage 를 통하여 sign 된 메시지가 제대로 sign 되었는지 검증 한다.
+ * @param client HDAC 네트워크 접속 정보를 제공한다.
+ * @param strAddress sign 할 때 사용 된 지갑 주소
+ * @param strSignmessage sign 되어진 문자열
+ * @param strText sign 한 원본 문자열
  *
  * @return verifymessage RPC 명령어의 결과 값, (검증 성공 여부, true, false)
  *
@@ -372,8 +359,8 @@ Object verifymessage(const RpcClient& client, const string &strAddress,
  * @brief json 형태의 RPC 명령어 결과를 string 형으로 변환 시켜 준다.
  * @details json 형태로 반환되는 RPC 명령어 결과 값들을 string 형으로
  * 변환 시켜 준다.
- * @param const Object & reply RPC 명령어로 반환 된 json 형태의 결과 값
- * @param string * resultStr string 형으로 변환 된 결과 값
+ * @param reply RPC 명령어로 반환 된 json 형태의 결과 값
+ * @param resultStr string 형으로 변환 된 결과 값
  *
  * @return json 형태에서 string 형으로 변환 성공 여부, true, false
  *
